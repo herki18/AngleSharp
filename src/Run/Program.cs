952bc66e453195e9f4f055ca51e77c4311b1fbf3
herki parn <herki77@gmail.com>
2024-10-12 15:23:55 +0100

    123

---
+++ src/Run/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Run;
+
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.ViewSync;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Create a new browsing context
+        var config = Configuration.Default;
+        var context = BrowsingContext.New(config);
+
+        // HTML content to parse
+        var source = "<!DOCTYPE html><html><head><title>Example</title></head><body><div id='content'>Hello, world!</div></body></html>";
+
+        // Parse the HTML content
+        var document = await context.OpenAsync(req => req.Content(source));
+
+        // Manipulate the DOM
+        var div = document.QuerySelector("#content");
+        if (div != null)
+        {
+            div.TextContent = "Hello, AngleSharp!";
+        }
+
+        // Output the modified HTML
+        Console.WriteLine(document.DocumentElement.OuterHtml);
+    }
+}
+
+public class VisualElement
+{
+
+}
+
+public class UnityViewSynchronizer : IViewSynchronizer
+{
+    public VisualElement Element;
+    public Node Node;
+
+    public UnityViewSynchronizer(VisualElement element, Node node)
+    {
+        Element = element;
+        Node = node;
+    }
+}
