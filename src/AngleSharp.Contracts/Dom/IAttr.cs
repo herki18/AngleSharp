b0c947e53fdd6f915753c92ea2a9265e9b86f940
herki parn <herki77@gmail.com>
2025-03-02 01:07:13 +0000

    213

--- src/AngleSharp.Contracts/Dom/IAttr.cs
+++ src/AngleSharp.Contracts/Dom/IAttr.cs
@@ -3,47 +3,20 @@ namespace AngleSharp.Dom;
 using System;
 using Attributes;
 
-/// <summary>
-///     This type represents a DOM element's attribute as an object.
-/// </summary>
 [DomName("Attr")]
 public interface IAttr : INode, IEquatable<IAttr>
 {
-    /// <summary>
-    ///     Gets the local name of the attribute.
-    /// </summary>
-    [DomName("localName")]
-    String LocalName { get; }
+    [DomName("localName")] String LocalName { get; }
 
-    /// <summary>
-    ///     Gets the attribute's name.
-    /// </summary>
-    [DomName("name")]
-    String Name { get; }
+    [DomName("name")] String Name { get; }
 
-    /// <summary>
-    ///     Gets the attribute's value.
-    /// </summary>
-    [DomName("value")]
-    String Value { get; set; }
+    [DomName("value")] String Value { get; set; }
 
-    /// <summary>
-    ///     Gets the namespace URL of the attribute.
-    /// </summary>
-    [DomName("namespaceURI")]
-    String? NamespaceUri { get; }
+    [DomName("namespaceURI")] String? NamespaceUri { get; }
 
-    /// <summary>
-    ///     Gets the prefix used by the namespace.
-    /// </summary>
-    [DomName("prefix")]
-    String? Prefix { get; }
+    [DomName("prefix")] String? Prefix { get; }
 
-    /// <summary>
-    ///     Gets the owning element, if any.
-    /// </summary>
-    [DomName("ownerElement")]
-    IElement? OwnerElement { get; }
+    [DomName("ownerElement")] IElement? OwnerElement { get; }
 
     /// <summary>
     ///     Gets always true.
--- src/AngleSharp.Contracts/Dom/IChildNode.cs
+++ src/AngleSharp.Contracts/Dom/IChildNode.cs
@@ -1,39 +1,24 @@
 namespace AngleSharp.Dom;
 
-using Attributes;
-
-/// <summary>
-///     The ChildNode interface contains methods that are particular to Node
-///     objects that can have a parent.
-/// </summary>
-[DomName("ChildNode")]
-[DomNoInterfaceObject]
-public interface IChildNode
-{
-    /// <summary>
-    ///     Inserts nodes just before the current node.
-    /// </summary>
-    /// <param name="nodes">The nodes to insert.</param>
-    [DomName("before")]
-    void Before(params INode[] nodes);
+    using Attributes;
 
     /// <summary>
-    ///     Inserts nodes just after the current node.
+    ///     The ChildNode interface contains methods that are particular to Node
+    ///     objects that can have a parent.
     /// </summary>
-    /// <param name="nodes">The nodes to insert.</param>
-    [DomName("after")]
-    void After(params INode[] nodes);
+    [DomName("ChildNode")]
+    [DomNoInterfaceObject]
+    public interface IChildNode
+    {
+        [DomName("before")]
+        void Before(params INode[] nodes);
 
-    /// <summary>
-    ///     Replaces the current node with nodes.
-    /// </summary>
-    /// <param name="nodes">The nodes to insert.</param>
-    [DomName("replace")]
-    void Replace(params INode[] nodes);
+        [DomName("after")]
+        void After(params INode[] nodes);
 
-    /// <summary>
-    ///     Removes the current node.
-    /// </summary>
-    [DomName("remove")]
-    void Remove();
-}
+        [DomName("replace")]
+        void Replace(params INode[] nodes);
+
+        [DomName("remove")]
+        void Remove();
+    }
