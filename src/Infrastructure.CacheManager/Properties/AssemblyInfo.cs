064ebf260d3206c3f2cd9114db8a6d393e3ac3ca
herki parn <herki77@gmail.com>
2025-03-24 15:42:34 +0000

    123

---
+++ src/Infrastructure.CacheManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCopyright("Copyright © AngleSharp, 2013-2024")]
+[assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("Infrastructure.CacheManager.Tests, PublicKey=BgIAAACkAABSU0ExAAgAAAEAAQDhZ/DA0bed0+H59L6r/r/bZzqw74vWSto6GDVTW9p0lzyA+i/UkBha1Lryue180VVNJFKfHDsFex/I4zrmP5yfgVrR1fTRyEahikQMLQlLGvQPDu0ERVVCe2dejJ9chqsPhQCIG8dzIejTvEfo9u5Ih2gOMSB1L28Q4ptIYfcxGZzu5USsa4EPs6QZKoB6YDGc1aZXhzOCkVIIaLUYvdvMiNkBDejpMcRu9H/5Cvt9kAbkJTirRikxr62y3yE/yhc+D94qqfXi2hfpY+jd4nNAqqkC1jT+g3jOnnrELdj9Zwf60Ocx1sVPg61bGOILbC/pHoCBSRxs+s6+uN5uYYqg")]
