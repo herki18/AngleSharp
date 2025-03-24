f016ba9bba0567e7379d626a1fb475c2e6da5dd1
herki parn <herki77@gmail.com>
2025-03-24 16:30:09 +0000

    123

---
+++ src/KeyExtractor/Program.cs
@@ -0,0 +1,29 @@
+﻿// using System;
+// using System.IO;
+// using System.Reflection;
+//
+// namespace KeyExtractor;
+//
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         // Path to your SNK file
+//         string keyFilePath = @"D:\Development\Github\AngleSharp\src\LayoutEngine.snk";
+//
+//         // Read the key file
+//         byte[] keyFileBytes = File.ReadAllBytes(keyFilePath);
+//
+//         // Create a StrongNameKeyPair from the key file
+//         var keyPair = new StrongNameKeyPair(keyFileBytes);
+//
+//         // Get the public key
+//         byte[] publicKey = keyPair.PublicKey;
+//
+//         // Convert to the format needed for InternalsVisibleTo
+//         string publicKeyString = BitConverter.ToString(publicKey).Replace("-", "");
+//
+//         Console.WriteLine("[assembly: InternalsVisibleTo(\"Infrastructure.CacheManager.Tests, PublicKey=" + publicKeyString + "\")]");
+//         Console.ReadLine();
+//     }
+// }
